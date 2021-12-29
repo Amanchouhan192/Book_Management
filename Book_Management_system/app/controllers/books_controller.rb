75ae3bda3356aea7e4276ff848baf6f467bb0af2
amanchouhan192 <amanchouhan192@gmail.com>
2021-12-29 14:44:27 +0530

    create book controller with new create index and show action.

---
+++ Book_Management_system/app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+    @authors = Author.all
+  end
+
+  def create
+    @book = Book.new(books_params)
+    if @book.save
+      redirect_to(books_path)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+    def books_params
+      params.require(:book).permit(:title, :publish_date, :author_id)
+    end
+end
